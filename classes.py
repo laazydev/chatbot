a2452cab51a68ebbf75adae8e0ccbf15bc2ccc81
Richard <laazydeveloper@gmail.com>
2022-12-03 12:37:30 +0000

    File Relocation

--- classes.py
+++
@@ -1,9 +0,0 @@
-# List of classes
-
-class Order:
-    def __init__(self, orderName, quantity):
-        self.orderName = orderName
-        self.quantity = quantity
-
-    def __str__(self):
-        return f'Your order is {self.quantity} of {self.orderName}'
