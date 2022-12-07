f42d491991b59e0edf374e6956f227c08f425eaf
Richard <laazydeveloper@gmail.com>
2022-12-07 19:45:41 +0000

    CheckPoint

---
+++ func/additionals.py
@@ -0,0 +1,5 @@
+# Additional Functions to make the code looks neater
+
+# Function to recheck the phone number
+def CP1_reprompt():
+    return 'hello'
--- func/functions.py
+++ func/functions.py
@@ -6,6 +6,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
+from tabulate import tabulate
+
 
 ## Function lists
 def NewCustomer(name, phone, address):
@@ -41,10 +43,47 @@ def checkPhone(number):
     # Error = 0
     # True = 1
 
+    # Starts with 0
     if number[0] == '0':
         number = number[1:]
+    elif number[0] == '+':
+        if number[1:3] == '44':
+            number = number[3:]
+            # This works
+    elif number[0:2] == '44':
+        number = number[2:]
     
     if len(number) == 10:
         return 1, number
     else:
         return 0, number
+
+def negationCheck(text):
+    negations = ["dont", "not", "never"]
+    for negs in negations:
+        if negs in text:
+            return True
+        else:
+            return False
+
+def isOrder(text):
+    orderTriggers = ['want', 'buy', 'order']
+    for ords in orderTriggers:
+        if ords in text:
+            return True
+        else:
+            return False
+
+def isPizza(text):
+    if 'pizza' in text:
+        return True
+    else:
+        return False
+
+def getPizzaList(data):
+    testinggg = []
+
+    for j in range(len(data)):
+        testinggg.append([data.iloc[j][1].capitalize(), data.iloc[j][10], data.iloc[j][3]])
+
+    return tabulate(testinggg, headers=["Pizza name", 'Price', 'Calories'])
